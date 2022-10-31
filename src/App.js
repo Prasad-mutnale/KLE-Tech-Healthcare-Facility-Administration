2d23b4eb0cb86c58f3dc1eba29f2fc8d91e5e01d
Prasad-mutnale <81313711+Prasad-mutnale@users.noreply.github.com>
2022-10-31 21:53:53 +0530

    3 files modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './style.css';
-
-export default function App() {
-  return <div>
-    
-  </div>;
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </>
+  );
 }
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div><h2>Contact page</h2></div>
+  )
+}
+
+export default Contact

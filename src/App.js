0920db146d464afb08c3cd7c12bc7fc884278e59
Prasad-mutnale <81313711+Prasad-mutnale@users.noreply.github.com>
2022-10-31 21:29:55 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
 }

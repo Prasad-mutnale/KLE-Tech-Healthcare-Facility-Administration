1f1bd42d08c67a561f8075057fdc7e5df2611e61
Prasad-mutnale <81313711+Prasad-mutnale@users.noreply.github.com>
2022-10-31 22:19:26 +0530

    3 files modified, 2 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';

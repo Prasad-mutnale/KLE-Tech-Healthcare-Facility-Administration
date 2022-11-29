96def69ee90916c704c875f0cdfb96926aae7d18
PrasadMutnale-exe <prasadmutnale1234@gmail.com>
2022-11-30 01:27:59 +0530

    login css code is added

--- src/comp1/Login.js
+++ src/comp1/Login.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
 import { useState } from 'react';
 import {useNavigate} from 'react-router-dom'
+import './Login.style.css'
 const Login = () => {
 
     const [Values,setdata]=useState({
@@ -46,13 +47,15 @@ if(result.usn){
   }
     return(
         <>
-      
-        <div className="login">
-            <h2>Login</h2>
-            <input className="inputBox" type="text" name="usn" value={Values.usn} onChange={setVal} placeholder="ENTER USN ID"/>
-            <input className="inputBox" type="password" name="password"  value={Values.password}  onChange={setVal}placeholder="ENTER PASSWORD"/>
-            <button onClick={handleSubmit} className="signup-btn" type='button'>Login</button>
-        </div>
+        <form>
+        <h3>Login Here</h3>
+        <label className="usnn" for="username">Usn Id</label>
+        <input className='login-input' type="text" name="usn" value={Values.usn} onChange={setVal} placeholder="ENTER USN ID"/>
+        <label className="pasww" for="password">Password(DOB)</label>
+        <input  className='login-input' type="password" name="password"  value={Values.password}  onChange={setVal}placeholder="DD/MM/YYY"/>
+        <button onClick={handleSubmit} className="login-btn" type='button'>Login</button>
+        </form>
+        
     </>
     )
 }

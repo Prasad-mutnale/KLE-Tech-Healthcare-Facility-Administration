eed002ee98064f2cc1f9b037814e763cfcacbf23
Prasad-mutnale <prasadmutnale1234@gmail.com>
2023-03-12 01:17:47 +0530

    font awesome removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
